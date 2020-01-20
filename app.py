a0e5b8a717074d536b6880fcd694757307ff6d09
marcuscolle <marcusvcolle@gmail.com>
2020-01-20 00:14:08 +0000

    missing edit products

--- app.py
+++ app.py
@@ -19,21 +19,34 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
+
 @app.route('/products')
 def products():
     return render_template("products.html", products=mongo.db.products.find())
 
 
+# route to add a product / this will open an new web window 
 @app.route('/add_product')
 def add_product():
     return render_template('addproduct.html', products=mongo.db.products.find())
 
-
+# this route will insert all the products input field into DB and return them in the product management page.
 @app.route('/insert_product', methods=['POST'])
 def insert_product():
     products = mongo.db.products
     products.insert_one(request.form.to_dict())
     return redirect(url_for('products'))
+
+
+# this will delete the data from products category/ will delete added products
+#should use product insted products 'cuz im looping product inside products list in mongodb.
+# only use products as mongodb to connect the DB to the page 
+@app.route('/delete_product/<product_id>')
+def delete_product(product_id):
+    mongo.db.products.remove({'_id': ObjectId(product_id)})
+    return redirect(url_for('products'))
+
+
 
 
 
